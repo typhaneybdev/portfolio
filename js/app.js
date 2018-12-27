6907bc63f27caaf8d99b4f2c0bb466e002d7fe87
Typhaney-Shanker <typhaney@gmail.com>
2018-12-27 15:16:44 -0700

    new sticky header

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//sticky header function
+window.onscroll = function() {
+  myFunction();
+};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+//https://www.w3schools.com/howto/howto_js_sticky_header.asp
