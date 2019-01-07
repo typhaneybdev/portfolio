4c648796b697aaaf8b8d5206a4b5becb093021d1
Typhaney-Shanker <typhaney@gmail.com>
2019-01-06 23:28:16 -0700

    about secition started

--- js/app.js
+++ js/app.js
@@ -15,4 +15,15 @@ function myFunction() {
 }
 //https://www.w3schools.com/howto/howto_js_sticky_header.asp
 
+//TODO: Hamburger menu toggle
+//document.querySelector(".fas fa-bars").addEventListener("click", menuToggle);
+//console.log("clicked");
 
+//function menuToggle() {
+// const x = document.getElementById("nav-menu");
+//if (x.className === "nav-menu") {
+// x.className += " responsive";
+// console.log("clicked");
+//} else {
+//   x.className = "nav-menu";
+// }
