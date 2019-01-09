ca9940b6e9db30a3718300cf9ae7568d22563c1f
Typhaney-Shanker <typhaney@gmail.com>
2019-01-08 23:32:20 -0700

    added jQuery smooth scroll

--- js/app.js
+++ js/app.js
@@ -15,6 +15,36 @@ function myFunction() {
 }
 //https://www.w3schools.com/howto/howto_js_sticky_header.asp
 
+
+
+//smooth scrolling https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_smooth_scroll_jquery
+$(document).ready(function() {
+  // Add smooth scrolling to all links
+  $("a").on("click", function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        1100,
+        function() {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
+
 //Toggle burger menu
 
 let mainNav = document.getElementById("js-menu");
